61b6f51c2b3e9124a391870bd727dde40cc65a06
Matthew S. MacLennan <msmaclennan@gmail.com>
2016-03-23 12:10:42 -0700

    Create gazette.gc.ca.r

---
+++ gazette.gc.ca.r
@@ -0,0 +1,5 @@
+#scouring for info on the Canada Gazette website
+#searching "public consultation" on the gazette website results sorted by date
+http://search2.gazette.gc.ca/search?access=p&entqr=0&output=xml_no_dtd&ie=UTF-8&lr=lang_en&client=gazette_frontend_clf3&
+q=%22public+consultation%22&filter=0&ud=1&gcwu-srch-submit=Search&oe=UTF-8&proxystylesheet=gazette_frontend_clf3&ip=137.82.65.131&
+hl=en&sort=date%3AD%3AS%3Ad1
