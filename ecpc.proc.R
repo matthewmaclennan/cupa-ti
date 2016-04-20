c825842c97c8ed92123f55d006bc37d35b12e1e3
Matthew S. MacLennan <msmaclennan@gmail.com>
2016-04-19 17:51:41 -0700

    added pie charts to pdf 2 x 2

--- ecpc.proc.R
+++ ecpc.proc.R
@@ -12,7 +12,11 @@ percent_complete<-as.matrix(as.numeric(timeleft)/as.numeric(range));colnames(per
 lbls<-paste0(round(as.numeric(ecpc[ecpc[,"time_left"]>0,][,"time_left"])/as.numeric(ecpc[ecpc[,"time_left"]>0,][,"range"])*100)," % left")
 piedata<-cbind(as.numeric(ecpc[ecpc[,"time_left"]>0,][,"time_left"]),as.numeric(ecpc[ecpc[,"time_left"]>0,][,"range"])-as.numeric(ecpc[ecpc[,"time_left"]>0,][,"time_left"]))
 
-#set up plot window since there are 25 results: 5 x 5.
+
+pdf(file=paste0(format(Sys.Date(),format="%Y%m%d"),".pdf"))
+#plot 4 pie charts per page
+par(mfrow=c(2,2))
 for(i in 1:length(lbls)){
  pie(piedata[i,],labels=lbls[i],col=c("green","red"))
  }
+dev.off()
